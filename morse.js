da7ad99a36ae2da4387eedcc5d5a99ce8381bd4a
Olli Rissanen <olli.rissanen@helsinki.fi>
2013-12-21 08:57:57 +0200

    Format

--- morse.js
+++ morse.js
@@ -1,18 +1,17 @@
-var tree = [' ', 'ET', 'IANM', 'SURWDKGO', 'HVFÜLÄPJBXCYZQÖ ', '54Ŝ3É  2 È+  ÀĴ16=/   Ĥ 7 ĜÑ8 90']
+var tree = [' ', 'ET', 'IANM', 'SURWDKGO', 'HVFÜLÄPJBXCYZQÖ ']
 var morsecode = "";
 
 function morse(signal) {
   if (signal === PAUSE) {
     return decode(morsecode);
   }
-  morsecode = morsecode.concat((signal === DIT) ? "." : "-");
+  morsecode += (signal === DIT) ? '0' : '1';
 }
 
 function decode(str){
-morsecode = "";
-return "".concat(tree[str.length].charAt(toint(str)));
-}
-
-function toint(str){ 
-  return parseInt(str.replace(/\./g,"0").replace(/\-/g,"1"), 2);
+  morsecode = "";
+  if (!str || str.length > 5){
+    return;
+  }
+  return tree[str.length].charAt(parseInt(str,2));
 }
